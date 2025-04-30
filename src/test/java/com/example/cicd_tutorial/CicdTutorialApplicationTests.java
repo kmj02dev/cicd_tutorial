08e8902d93934b0a3ffda5e3d8d79fa22a738849
kmj02dev <kmj02dev@gmail.com>
2025-04-30 12:48:57 +0900

    first ci

---
+++ src/test/java/com/example/cicd_tutorial/CicdTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
